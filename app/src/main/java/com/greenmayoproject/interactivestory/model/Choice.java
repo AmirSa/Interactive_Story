59214b8660eeb0591bc0ad68d3991e1b2762b5b2
Amir Salameh <amir.a.salameh@gmail.com>
2015-04-05 11:39:16 +0100

    Initial commit

---
+++ app/src/main/java/com/greenmayoproject/interactivestory/model/Choice.java
@@ -0,0 +1,25 @@
+package com.greenmayoproject.interactivestory.model;
+
+/**
+ * Created by Amir on 4/4/2015.
+ */
+public class Choice {
+    private String mText;
+    private int mNextPage;
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+
+    public int getNextPage() {
+        return mNextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        mNextPage = nextPage;
+    }
+}
---
+++ app/src/main/java/com/greenmayoproject/interactivestory/ui/StoryActivity.java
@@ -0,0 +1,25 @@
+package com.greenmayoproject.interactivestory.ui;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.greenmayoproject.interactivestory.R;
+
+
+public class StoryActivity extends ActionBarActivity {
+
+    public static final String TAG = StoryActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra(getString(R.string.key_name));
+
+        Log.d(TAG, name);
+    }
+}
